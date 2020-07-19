68efff456a0e6d214e3c113e389e0a8fdb80f72b
MIKI-UK\s.nathan <s.nathan@group-miki.com>
2020-07-19 12:31:00 +0100

    Added negative case and updated postive case to assert meaningful result

--- src/test/java/messageprocessor/handler/processor/ProcessorTest.java
+++ src/test/java/messageprocessor/handler/processor/ProcessorTest.java
@@ -2,12 +2,16 @@ package messageprocessor.handler.processor;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import messageprocessor.dto.Constants;
 import messageprocessor.dto.Input;
+import messageprocessor.dto.Result;
+import messageprocessor.exception.InvalidMessageException;
 import messageprocessor.processor.Processor;
 import org.junit.Before;
 import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
-
+import static org.junit.Assert.assertTrue;
 
 /**
  * Created by s.nathan on 19/07/2020.
@@ -27,6 +31,21 @@ public class ProcessorTest {
         String input = getInput();
         String output = processor.processMessage(input);
         assertNotNull(" Unexpected output ", output);
+        Result result = toResult(output);
+        assertTrue(" Wrong result ", result.getResponse().length == 16);
+    }
+
+    @Test(expected = InvalidMessageException.class)
+    public void testProcessMessage_InvalidInput() {
+        String input = "{A,B,C,D}";
+        String output = processor.processMessage(input);
+        assertNotNull(" Unexpected output ", output);
+        try{
+            Result result = toResult(output);
+        }catch(InvalidMessageException e) {
+            assertEquals(" Wrong output ", Constants.INVALID_MESSAGE, e.getMessage());
+            throw e;
+        }
     }
 
     private String getInput(){
@@ -34,5 +53,9 @@ public class ProcessorTest {
         Input input = new Input();
         input.setInput(inputArray);
         return gson.toJson(input);
+    }
+
+    private Result toResult(String output) {
+        return gson.fromJson(output, Result.class);
     }
 }
