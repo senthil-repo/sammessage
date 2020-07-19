50d9aff2f030b1af5d60e51a3b4f74be412d99d0
MIKI-UK\s.nathan <s.nathan@group-miki.com>
2020-07-19 11:28:14 +0100

    Ran the postive case with basic implementation successfully

--- src/main/java/messageprocessor/handler/MessageHandler.java
+++ src/main/java/messageprocessor/handler/MessageHandler.java
@@ -1,13 +1,23 @@
 package messageprocessor.handler;
 
 import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.SQSEvent;
 
 /**
  * Created by s.nathan on 19/07/2020.
  */
-public class MessageHandler {
+public class MessageHandler implements RequestHandler<SQSEvent, String> {
+    @Override
     public String handleRequest(SQSEvent sqsEvent, Context context) {
-        return "";
+        String message = null;
+        String messageId = null;
+
+        if(sqsEvent != null && sqsEvent.getRecords() != null) {
+            for(SQSEvent.SQSMessage msg : sqsEvent.getRecords()){
+            }
+        }
+
+        return "Success";
     }
 }
