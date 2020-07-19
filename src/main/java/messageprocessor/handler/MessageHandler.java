35dc2a0563e5d5790b8dc96420363b2e7c884a8a
MIKI-UK\s.nathan <s.nathan@group-miki.com>
2020-07-19 13:02:56 +0100

    Implemented negative case

--- src/main/java/messageprocessor/handler/MessageHandler.java
+++ src/main/java/messageprocessor/handler/MessageHandler.java
@@ -10,6 +10,7 @@ import messageprocessor.writer.Writer;
 import org.apache.commons.lang3.StringUtils;
 
 /**
+ * Main Lambda class that gets invoked whenever a message is dropped in SQS
  * Created by s.nathan on 19/07/2020.
  */
 public class MessageHandler implements RequestHandler<SQSEvent, String> {
--- src/main/java/messageprocessor/writer/Writer.java
+++ src/main/java/messageprocessor/writer/Writer.java
@@ -5,24 +5,43 @@ import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.dynamodbv2.document.DynamoDB;
 import com.amazonaws.services.dynamodbv2.document.Item;
 import com.amazonaws.services.dynamodbv2.document.Table;
+import messageprocessor.dto.Constants;
+import messageprocessor.exception.UnableToStoreOutputException;
 
 /**
+ * Class to store the result to dynamodb table
  * Created by s.nathan on 19/07/2020.
  */
 public class Writer {
-    public void storeOutput(String messageId, String processedMessage) {
-        AmazonDynamoDB amazonDynamoDB = AmazonDynamoDBClientBuilder.defaultClient();
-        DynamoDB dynamoDB = new DynamoDB(amazonDynamoDB);
-        Table table = dynamoDB.getTable("processedmessage");
-        Item item = new Item()
-                .withPrimaryKey("MessageId", messageId)
-                .withStringSet("output", processedMessage);
+    public void storeOutput(String messageId, String processedMessage)  {
+        StringBuilder stringBuilder;
+        try {
+            AmazonDynamoDB amazonDynamoDB = AmazonDynamoDBClientBuilder.defaultClient();
+            DynamoDB dynamoDB = new DynamoDB(amazonDynamoDB);
+            Table table = dynamoDB.getTable("processedmessage");
+            Item item = new Item()
+                    .withPrimaryKey("MessageId", messageId)
+                    .withStringSet("output", processedMessage);
 
-        StringBuilder stringBuilder = new StringBuilder(" Table name: ")
+            printItem(table, item);
+
+            table.putItem(item);
+        } catch(Exception e) { //This is caught just to give a meaningful exception to the caller,
+            // in case of any issues while connecting to the database
+            stringBuilder = new StringBuilder(" Unable to process the message for message id: ")
+                    .append(messageId)
+                    .append(" - ")
+                    .append(e.getMessage());
+            System.out.println(stringBuilder.toString());
+            throw new UnableToStoreOutputException(Constants.UNABLE_TO_STORE_OUTPUT);
+        }
+    }
+
+    private void printItem(Table table, Item item) {
+        StringBuilder stringBuilder;
+        stringBuilder = new StringBuilder(" Table name: ")
                 .append(table.getTableName())
                 .append(" Item details: ").append(item.toJSONPretty());
         System.out.println(stringBuilder.toString());
-
-        table.putItem(item);
     }
 }
